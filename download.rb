0fe3042819e0bb896fdb283e430e8e51988fd7b2
James Pellerano <james@japellerano.com>
2013-04-25 23:59:06 -0400

    updated estel to work

--- download.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'open-uri'
-
-filename = 'master.zip'
-stream = 'https://github.com/japellerano/Web-Development-Template/archive/master.zip'
-
-open(filename, 'wb').write(open(stream).read)
-
---
+++ estel.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'uri'
+
+filename = 'master.zip'
+stream = 'https://github.com/japellerano/Web-Development-Template/archive/master.zip'
+
+# Methods
+def separate_url(url)
+  uri = URI(url)
+
+  s = url.split("/")
+  l = s.length
+  return s[l - 1]
+end
+
+def download(filename, url)
+  open(filename, 'wb').write(open(url).read)
+end
+
+puts "Welcome to Estel, please enter the URL of the file you would like to download: "
+path = gets.chomp!
+
+download(separate_url(path), path)
